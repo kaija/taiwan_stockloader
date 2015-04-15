26c76b8d534554edd662293daef2f50376720299
kaija <kaija.chang@gmail.com>
2015-04-15 11:04:06 +0800

    add initial version of taiwan stock csv downloader

---
+++ stockloader.js
@@ -0,0 +1,82 @@
+var http = require('http');
+var fs = require('fs');
+var querystring = require('querystring');
+var Promise = require('promise');
+
+console.log("Start download stock information...");
+var day_diff = 60 * 60 * 24 * 1000;
+
+//start time
+var start_ts = new Date("2015/01/01").getTime()
+//end time
+var stop_ts = new Date().getTime();
+
+function twoDigit(num)
+{
+	if(num < 10){
+		return '0' + num;
+	}
+	return num;
+}
+
+function saveStock(data)
+{
+	//console.log("Date:" + data.body);
+	fs.writeFile(data.file, data.body,function(err){
+		if(err){
+			console.log(err);
+		}else{
+			console.log("File " + data.file + " download.");
+		}
+	});
+}
+
+function downloadStock(start_ts)
+{
+	var date = new Date(start_ts);
+	return new Promise(function(fullfill, reject){
+		var tw_year = date.getFullYear() - 1911;
+		var tw_month =  date.getMonth() + 1;
+		var tw_day = date.getDate();
+		var date_str = tw_year.toString() + '/' + twoDigit(tw_month) + '/' + twoDigit(tw_day);
+		var postData = querystring.stringify({
+			'download': 'csv',
+			'qdate': date_str,
+			'selectType': 'ALLBUT0999'
+		});
+		console.log(postData);
+		var fullYear = date.getFullYear().toString();
+		var file_str = fullYear + twoDigit(tw_month) + twoDigit(tw_day)+'.csv';
+		console.log(file_str);
+		var options = {
+			hostname:'www.tse.com.tw',
+			port: 80,
+			path:'/ch/trading/exchange/MI_INDEX/MI_INDEX.php',
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				'Content-Length': postData.length
+			}
+		};
+		var req = http.request(options, function(res){
+			res.setEncoding('binary');
+			var data = {file:file_str, body:''};
+			var body = '';
+			res.on('data', function(chunk){
+				//console.log(chunk);
+				body+=chunk;
+			});
+			res.on('end', function(){
+				data.body = body;
+				fullfill(data);
+			});
+		});
+		req.write(postData);
+		req.end();
+	});
+}
+
+while(start_ts < stop_ts){
+	downloadStock(start_ts).then(saveStock);
+	start_ts += day_diff;
+}
