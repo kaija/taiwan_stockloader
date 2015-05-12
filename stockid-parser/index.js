bdc469927a50981c5c614be25bf9cd6da31ccd5a
Kaija <kaija.chang@gmail.com>
2015-05-12 20:44:42 +0800

    Update stock id import to elasticsearch

--- stockid-parser/index.js
+++ stockid-parser/index.js
@@ -1,8 +1,31 @@
 var fs = require('fs');
+var esIndex = 'stock';
+var esType = 'stockname';
+var elasticsearch = require('elasticsearch');
+var poolModule = require('generic-pool');
+var pool = poolModule.Pool({
+  name:'elasticsearch',
+  create: function(callback){
+    var cli = new elasticsearch.Client({
+      host:'localhost:9200',
+      log: 'trace'
+    });
+    callback(null, cli);
+  },
+  destroy: function(client){
+  },
+  min: 100,
+  max: 1000,
+  refreshIdle: false,
+  log: false
+});
+
+var stocklist = [];
+
 var raw = fs.readFileSync('stocklist.txt');
 if(raw){
   var rows = raw.toString().split('\n');
-  for(var i in rows){
+  for(i in rows){
     var line = rows[i].split(',');
     var id = line[0].trim();
     var name = line[1];
@@ -12,7 +35,26 @@ if(raw){
       stock['id'] = id;
       stock['name'] = name;
       stock['category'] = cat;
-      console.log(stock);
+      stocklist.push(stock);
     }
   }
 }
+
+
+for (j in stocklist){
+  //console.log(stocklist[i]);
+  pool.acquire(function(err, cli){
+    var o = {
+      index: esIndex,
+      type: esType,
+      body: stocklist[j]
+    };
+    console.log(o);
+    cli.create(o, function(err, res){
+      //console.log(stocklist[j].id + " save to ES");
+      //console.log(res);
+      pool.release(cli);
+    });
+  });
+}
+
