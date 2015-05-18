9e7852b3ea9c2ade29049e6cf55b007be235f4a0
Kaija <kaija.chang@gmail.com>
2015-05-18 09:32:30 +0800

    import data to redis one by one avoid OOM

--- import.py
+++ import.py
@@ -8,6 +8,7 @@ from datetime import timedelta
 #now = datetime.datetime.now();
 #today = now.strftime('%Y-%m-%d')
 #print today
+rdb = redis.Redis('localhost')
 
 
 def isfloat(value):
@@ -29,11 +30,25 @@ def convint(value):
 	except ValueError:
 		return 0
 
+
+def save2redis(key, value):
+	old = rdb.get("TW" + key)
+	if old is None:
+		val = []
+		val.append(value)
+		rdb.set("TW"+key ,json.dumps(val))
+	else:
+		l = json.loads(old)
+		l.append(value)
+		rdb.set("TW"+key ,json.dumps(l))
+		
+
+
 today = datetime.date.today()
 one_day = timedelta(days=1);
 
 start_day = datetime.date(2004, 2, 11);
-#start_day = datetime.date(2015, 1, 11);
+#start_day = datetime.date(2015, 5, 14);
 
 print "Import from " + start_day.strftime("%Y-%m-%d") + " to " + today.strftime("%Y-%m-%d")
 
@@ -58,18 +73,8 @@ while dl_date < today:
 			#print head[1] + " " + r[2] + " " + convfloat(r[5])
 			#print r[2] #volume
 			#print r[5] #open
-			if sid in stocks:
-				stocks[sid].append({"volume": convint(r[2]), "open": convfloat(r[5]), "high": convfloat(r[6]), "low": convfloat(r[7]), "val": convfloat(r[8]), "date": dl_date.strftime("%Y-%m-%d"), "per": convfloat(r[15]), "buyQuantity": convint(r[12]), "buyPrice": convint(r[11]), "saleQuantity": convint(r[14]), "salePrice": convint(r[13])})
-			else:
-				stocks[sid] = []
-				stocks[sid].append({"volume": convint(r[2]), "open": convfloat(r[5]), "high": convfloat(r[6]), "low": convfloat(r[7]), "val": convfloat(r[8]), "date": dl_date.strftime("%Y-%m-%d"), "per": convfloat(r[15]), "buyQuantity": convint(r[12]), "buyPrice": convint(r[11]), "saleQuantity": convint(r[14]), "salePrice": convint(r[13])})
+			obj = {"volume": convint(r[2]), "open": convfloat(r[5]), "high": convfloat(r[6]), "low": convfloat(r[7]), "val": convfloat(r[8]), "date": dl_date.strftime("%Y-%m-%d"), "per": convfloat(r[15]), "buyQuantity": convint(r[12]), "buyPrice": convint(r[11]), "saleQuantity": convint(r[14]), "salePrice": convint(r[13])}
+			save2redis(sid, obj)
 
 	dl_date += one_day
 
-print "Start import to Redis"
-
-
-rdb = redis.Redis('localhost')
-for i in iter(stocks):
-	sid = "TW" + i
-	rdb.set(sid, json.dumps(stocks[i]))
